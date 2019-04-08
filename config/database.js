9c3525cde132db64a6cb3331ba9834364108ab82
David Arthur <arthur.assan@gmail.com>
2019-04-08 23:27:48 +0000

    remote db configuration

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost/itendax',
+    // database: 'mongodb://localhost/itendax',
+    database: 'mongodb://admin:admin123@ds135036.mlab.com:35036/itendax',
     secret: 'shitalwayshappens'
 }
