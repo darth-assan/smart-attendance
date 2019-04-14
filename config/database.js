3962bfff605885d75b42dd660d4e512f36dd7ca0
Arthur Assan David <assan@localhost.localdomain>
2019-04-14 17:17:54 -0400

    provisioning for heroku hosting

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     // Local database
-    database: 'mongodb://localhost/itendax',
+    // database: 'mongodb://localhost/itendax',
     // Remote database
-    // database: 'mongodb://admin:admin123@ds135036.mlab.com:35036/itendax',
+    database: 'mongodb://admin:admin123@ds135036.mlab.com:35036/itendax',
     secret: '4zG#e,ySEy>Nphkuh9{5"4@.56M7do'
 }
